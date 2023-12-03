67c7513729af7f84ebb992472e33656da7801b4c
aquitan <aquitan@mail.ru>
2023-12-03 23:41:23 +0300

    auth middleware

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,21 +1,55 @@
 const ApiError = require("../error/ApiError")
+const bcrypt = require('bcrypt')
+const { User, Basket } = require('../models/models')
+const jwt = require('jsonwebtoken')
 
-const registration = (async (req, res) => {
+
+const generateJwt = (id, email, role) => {
+	return jwt.sign({ id, email, role }, process.env.SECRET_KEY, { expiresIn: '24h' })
+}
+
+const registration = (async (req, res, next) => {
+	const { email, password, role } = req.body
+
+	if (!email || !password) {
+		return next(ApiError.internalError('Неверно указаны логин или пароль'))
+	}
+	const candidate = await User.findOne({ where: { email } })
+
+	if (candidate) {
+		return next(ApiError.internalError('Пользователь с таким email уже существует'))
+	}
+
+	const hashPassword = await bcrypt.hash(password, 5)
+	const user = await User.create({ email, password: hashPassword, role })
+	const basket = await Basket.create({ userId: user.id })
+	const token = generateJwt(user.id, user.email, user.role)
+
+	return res.json({ token })
 
 })
 
-const login = (async (req, res) => {
+const login = (async (req, res, next) => {
+	const { email, password } = req.body
+	const user = await User.findOne({ where: { email } })
+
+	if (!user) {
+		return next(ApiError.internalError('Пользователь с таким email не найден'))
+	}
+
+	let comparePassword = bcrypt.compareSync(password, user.password)
+	if (!comparePassword) {
+		return next(ApiError.internalError('Неверный пароль'))
+	}
+
+	const token = generateJwt(user.id, user.email, user.role)
+
+	return res.json({ token })
 
 })
 
 const check = (async (req, res, next) => {
-	const { id } = req.query
 
-	if (!id) {
-		return next(ApiError.badRequest('Не задан id'))
-	}
-
-	res.json({ message: 'Success' })
 })
 
 module.exports = { registration, login, check }
